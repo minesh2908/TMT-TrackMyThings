54b7e17b995a4825569eeb583f0e82621f566723
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-07-03 17:47:16 +0530

    minor update

--- lib/service/shared_prefrence.dart
+++ lib/service/shared_prefrence.dart
@@ -200,5 +200,7 @@ class AppPrefHelper {
     await AppPref.remove(AppPrefKey.email);
     await AppPref.remove(AppPrefKey.phoneNumber);
     await AppPref.remove(AppPrefKey.defaultWarrantyPeriod);
+    await AppPrefHelper.setLanguage(language: 'en');
+  
   }
 }
