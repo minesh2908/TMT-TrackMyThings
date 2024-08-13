07610e0814092369a1a616360840280f9c2501fe
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-13 22:33:23 +0530

    Function test

---
+++ functions/notification.js
@@ -0,0 +1,16 @@
+exports.handler = async (event, context)=>{
+    try {
+      console.log('I am in Try');
+      return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Notification scheduled successfully' })
+      };
+    } catch (error) {
+      console.log(`Here is error :  ${error}`);
+      
+      return {
+          statusCode: 502,
+          body: JSON.stringify({ message: 'Notification scheduled failed', error:error})
+      };
+    }
+  }
