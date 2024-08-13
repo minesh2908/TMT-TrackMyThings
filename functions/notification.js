6bd965323b5986b2be7ddb1f87ab1436c262a25e
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-13 22:49:02 +0530

    function testing

--- functions/notification.js
+++ functions/notification.js
@@ -2,8 +2,11 @@ const admin = require('firebase-admin');
 
 exports.handler = async (event, context)=>{
     try {
+     const serviceAccount = process.env.SERVICE_ACCOUNT_KEY;
       console.log('I am in Try');
+      console.log(`service Account : ${serviceAccount}`);
       return {
+         
           statusCode: 200,
           body: JSON.stringify({ message: 'Notification scheduled successfully' })
       };
