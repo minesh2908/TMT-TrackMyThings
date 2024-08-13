b9554505be4cfed52e190d6358320d53882ba9ca
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-13 23:08:16 +0530

    Update notification.js

--- functions/notification.js
+++ functions/notification.js
@@ -1,23 +1,30 @@
-const admin = require('firebase-admin'); 
+const admin = require("firebase-admin");
 
-exports.handler = async (event, context)=>{
-    try {
-        const serviceAccount = process.env.SERVICE_ACCOUNT_KEY;
-        admin.initializeApp({credential:admin.credential.cert(serviceAccount),});
-        const firestore = admin.firestore();
-        console.log(`firestore: ${firestore}`);
-        const snapshot = await firestore.collection('productCollection').get();
-        console.log(`snapshot data: ${snapshot}`);
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ message: 'Notification scheduled successfully' })
-      };
-    } catch (error) {
-      console.log(`Here is error :  ${error}`);
-      
-      return {
-          statusCode: 502,
-          body: JSON.stringify({ message: 'Notification scheduled failed', error:error})
-      };
-    }
+exports.handler = async (event, context) => {
+  try {
+    const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+    
+    admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+    
+    const firestore = admin.firestore();
+    console.log(`firestore: ${firestore}`);
+    
+    const snapshot = await firestore.collection("productCollection").get();
+    console.log(`snapshot data: ${snapshot}`);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Notification scheduled successfully" }),
+    };
+  } catch (error) {
+    console.log(`Here is error :  ${error}`);
+
+    return {
+      statusCode: 502,
+      body: JSON.stringify({
+        message: "Notification scheduled failed",
+        error: error,
+      }),
+    };
   }
+};
