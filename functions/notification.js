e26b6820890090f3feee3a50a46dbdaa3ed0e0cd
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-15 23:14:51 +0530

    Update notification.js

--- functions/notification.js
+++ functions/notification.js
@@ -11,24 +11,21 @@ if (!admin.apps.length) {
 
 exports.handler = async (event, context) => {
   try {
-    const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
-    
-    admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
-    
     const firestore = admin.firestore();
     console.log(`firestore: ${firestore}`);
     
     const snapshot = await firestore.collection("productCollection").get();
-    // console.log(`snapshot data: ${snapshot}`);
 
+    // Construct the notification message
     const message = {
       notification: {
         title: "Warranty Expiry Reminder",
         body: `Your warranty for Product is expiring in 3 days.`,
       },
-      token: 'e4Iy6ZNtR1-Txbv5iOcg46:APA91bF9waq7uqAhj9iJe1Tfmth-HH8sx7ZkN_1nlIgSfFbAtc-9fXfdXkPfpszpjaYurQoFbFb07OsT5_IYg6W-4yI4ERrAGVOFoFy4gV2domqMrDvZTsckVT9Enq12tyW9pn_QTwDD', // Assuming you have stored the user's FCM device token in Firestore
+      token: 'e4Iy6ZNtR1-Txbv5iOcg46:APA91bF9waq7uqAhj9iJe1Tfmth-HH8sx7ZkN_1nlIgSfFbAtc-9fXfdXkPfpszpjaYurQoFbFb07OsT5_IYg6W-4yI4ERrAGVOFoFy4gV2domqMrDvZTsckVT9Enq12tyW9pn_QTwDD', // Use the FCM token from Firestore if available
     };
     
+    // Send the notification
     const response = await admin.messaging().send(message);
     console.log(`Notification sent: ${response}`);
     
