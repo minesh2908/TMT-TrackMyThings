03f7366ade1f21d6c359283879f04e08fe4519b6
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2024-08-17 17:46:05 +0530

    Update firebase.js

--- functions/firebase.js
+++ functions/firebase.js
@@ -2,14 +2,21 @@ const admin = require("firebase-admin");
 
 let app;
 
+// Check if the Firebase app has already been initialized
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
-  app = admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+  
+  // Initialize the app with the service account credentials
+  app = admin.initializeApp({ 
+    credential: admin.credential.cert(serviceAccount) 
+  });
 } else {
   app = admin.app();
 }
 
+// Initialize Firestore and Cloud Messaging using the app instance
 const db = app.firestore();
-const messaging = app.messaging(); 
+const messaging = app.messaging();
 
-module.exports = {db, messaging};
+// Export the Firestore and Cloud Messaging instances for use in other files
+module.exports = { db, messaging };
