76181fa6c33752db839907a661ae0acaf247d929
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2024-08-17 17:46:36 +0530

    Update notificationService.js

--- functions/notificationService.js
+++ functions/notificationService.js
@@ -1,15 +1,10 @@
-// Import the retrieveProductData function correctly using require
 const { retrieveProductData } = require('./retrieveData');
 
 exports.handler = async (event, context) => {
     try {
-        // Await the async function to get the product data
         const productData = await retrieveProductData();
-        
-        // Log the product data properly
         console.log('Product Data:', productData);
         
-        // Optionally, return the product data in your response if this is an API handler
         return {
             statusCode: 200,
             body: JSON.stringify(productData),
