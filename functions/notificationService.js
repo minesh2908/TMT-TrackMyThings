07718c10bbbaaef03b7005011646f7f14c555cc2
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-17 23:53:31 +0530

    Update notificationService.js

--- functions/notificationService.js
+++ functions/notificationService.js
@@ -1,16 +1,22 @@
-const { retrieveProductData } = require('./retrieveData');
+const { retrieveProductData, getUserToken } = require('./retrieveData');
 
 exports.handler = async (event, context) => {
     try {
         
         const productData30 = await retrieveProductData(30);
         console.log('Product Data with 30 day warranty:', productData30);
-        
+        const userTokens30 = await getUserToken(productData30);
+        console.log('Push Tokens: ', userTokens30)
+
         const productData7 = await retrieveProductData(7);
         console.log('Product Data with 7 day warranty:', productData7);
+        const userTokens7 = await getUserToken(productData7);
+        console.log('Push Tokens: ', userTokens7)
 
         const productData3 = await retrieveProductData(3);
         console.log('Product Data with 3 day warranty:', productData3);
+        const userTokens3 = await getUserToken(productData3);
+        console.log('Push Tokens: ', userTokens3)
 
         return {
             statusCode: 200,
