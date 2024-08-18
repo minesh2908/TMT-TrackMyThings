b7d02fabb6baa5da8bb3c69666eb144b3e4c7f98
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-18 15:35:13 +0530

    Update retrieveUserToken.js

--- functions/retrieveUserToken.js
+++ functions/retrieveUserToken.js
@@ -3,13 +3,12 @@ const { db } = require('./firebase.js');
 async function getUserToken(userId) {
     try {
         console.log(`1 ${userId}`);
-        console.log("done");
 
         // Query Firestore to get the user document with the specified userId
         const userSnapshot = await db.collection("userCollection")
             .where("userId", "==", "oP7gUH8w9wZqQaDnKgwmU1xkHn33")
             .get();
-
+         console.log(userSnapshot);
         if (userSnapshot.empty) {
             console.log(`No user found with userId: ${userId}`);
             return null; // Return null if no user is found
