afb0f7de798a6db8700a8d22cd8ebc0ac0735f44
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-18 17:10:51 +0530

    Update sendNotification.js

--- functions/sendNotification.js
+++ functions/sendNotification.js
@@ -3,25 +3,20 @@ const { messaging } = require('./firebase.js');
 async function sendNotification(pushToken, productData){
     const message = {
         notification: {
-
           title: "Warranty Expiry Reminder",
           body: `Your warranty for ${productData.productName} is expiring in 3 days.`,
-          image:productData.productImage
+          image: productData.productImage
         },
         token: pushToken,
       };
 
       // Send the notification
-      promises.push(messaging.send(message)
-        .then(response => {
-          console.log(`Notification sent to ${pushToken} for product ${productData.productName}: ${response}`);
-        })
-        .catch(error => {
-          console.error(`Error sending notification to ${pushToken}:`, error);
-        }));
-
+      try {
+        const response = await messaging.send(message);
+        console.log(`Notification sent to ${pushToken} for product ${productData.productName}: ${response}`);
+      } catch (error) {
+        console.error(`Error sending notification to ${pushToken}:`, error);
+      }
 }
 
 module.exports = { sendNotification };
-
-
