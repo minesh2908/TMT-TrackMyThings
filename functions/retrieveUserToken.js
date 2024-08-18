2f440e8c0c61b03fa6ae4c05bb408fd01f9bee50
Minesh Sarawogi <54062823+minesh2908@users.noreply.github.com>
2024-08-18 15:31:40 +0530

    Update retrieveUserToken.js

--- functions/retrieveUserToken.js
+++ functions/retrieveUserToken.js
@@ -3,31 +3,27 @@ const { db } = require('./firebase.js');
 async function getUserToken(userId) {
     try {
         console.log(`1 ${userId}`);
-         console.log("done");
-         let userDoc;
-         userDoc = await db.collection("userCollection")
-                .where("userId", "==", userId)
-                .get();
-        if (userDoc.empty) {
+        console.log("done");
+
+        // Query Firestore to get the user document with the specified userId
+        const userSnapshot = await db.collection("userCollection")
+            .where("userId", "==", userId)
+            .get();
+
+        if (userSnapshot.empty) {
             console.log(`No user found with userId: ${userId}`);
             return null; // Return null if no user is found
         }
-         
-        const users = [];
-        userDoc.forEach(doc => {
-            users.push({ id: doc.id, ...doc.data() });
-        });
-        
-        console.log(users.length);
 
-        console.log(`userDoc: ${userDoc}`)
-        const userData = userDoc.data();
-        console.log(`userData: ${userData}`);   
-        
-        const pushToken = userData.pushToken;
-        console.log(`pushToken: ${pushToken}`);
-          // Return the pushToken and productImage
-        
+        let pushToken;
+        userSnapshot.forEach(doc => {
+            const userData = doc.data();
+            console.log(`userData: ${JSON.stringify(userData)}`);
+            pushToken = userData.pushToken; // Assuming 'pushToken' is the field name in Firestore
+            console.log(`pushToken: ${pushToken}`);
+        });
+
+        // Return the pushToken
         return pushToken;
     } catch (error) {
         console.log(`Error: ${error}`);
